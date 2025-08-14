04878bf18667edb17e8dd8160cdb6e512920c1f5
Chrimle <28791817+Chrimle@users.noreply.github.com>
2025-08-15 00:26:36 +0200

    Add Utility Method for Calculating Fewest Bills for Sum

--- scripts/bills.js
+++ scripts/bills.js
@@ -8,3 +8,19 @@ export function getBillDenomination(bill) {
 export function getValueSum(bills) {
     return bills.map(getBillDenomination).reduce((sum, val) => sum + val, 0);
 }
+
+export function getFewestBillsForSum(sum) {
+
+    let numberOfBills = 0;
+    let highestDenominationIndex = BILL_DENOMINATIONS.length - 1;
+
+    while (sum > 0) {
+        if (BILL_DENOMINATIONS[highestDenominationIndex] <= sum) {
+            sum -= BILL_DENOMINATIONS[highestDenominationIndex];
+            numberOfBills++;
+        } else {
+            highestDenominationIndex--;
+        }
+    }
+    return numberOfBills;
+}
