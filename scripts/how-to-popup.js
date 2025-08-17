98bd831ee6196acd78fc8a3a9d1b28297038033c
Chrimle <28791817+Chrimle@users.noreply.github.com>
2025-08-17 17:00:16 +0200

    Show 'How to Play' on First Visit

--- scripts/how-to-popup.js
+++ scripts/how-to-popup.js
@@ -1,4 +1,6 @@
 
+const HAS_READ_HOW_TO = 'has-read-how-to';
+
 export function setupHowToPopup() {
 
     document.getElementById('info-btn').addEventListener('click', function () {
@@ -12,4 +14,9 @@ export function setupHowToPopup() {
     document.getElementById('howto-popup').addEventListener('click', function (e) {
         if (e.target === this) this.hidden = true;
     });
+
+    if (localStorage.getItem(HAS_READ_HOW_TO) === null) {
+        document.getElementById('howto-popup').hidden = false;
+        localStorage.setItem(HAS_READ_HOW_TO, 'true');
+    }
 }
