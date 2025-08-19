406374255563c6007ad3e6f0e00f8d8fe3e6c1ed
Chrimle <28791817+Chrimle@users.noreply.github.com>
2025-08-20 00:21:30 +0200

    Import 'Cookie Banner' CSS via Script Only When Needed

--- resources/modules/cookie-banner/script.js
+++ resources/modules/cookie-banner/script.js
@@ -3,4 +3,12 @@ export async function insertCookieBannerTemplate() {
     const html = await response.text();
     document.body.insertAdjacentHTML("beforeend", html);
     document.getElementById("analytics-banner").style.display = "flex";
+
+    if (!document.getElementById("cookie-banner-style")) {
+        const link = document.createElement("link");
+        link.id = "cookie-banner-style";
+        link.rel = "stylesheet";
+        link.href = "./resources/modules/cookie-banner/style.css";
+        document.head.appendChild(link);
+    }
 }
