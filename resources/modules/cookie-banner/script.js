45b89fd2a24a41339e873205e9a622256442bb36
Chrimle <28791817+Chrimle@users.noreply.github.com>
2025-08-19 23:51:19 +0200

    Keep the 'Cookie Banner' Hidden when Loaded

--- resources/modules/cookie-banner/script.js
+++ resources/modules/cookie-banner/script.js
@@ -2,5 +2,4 @@ fetch("./resources/modules/cookie-banner/template.html")
   .then(r => r.text())
   .then(html => {
     document.body.insertAdjacentHTML("beforeend", html);
-    document.getElementById("analytics-banner").style.display = "flex";
   });
