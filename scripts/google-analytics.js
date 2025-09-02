2923fd92974d64e8a0573381961010e6b1cf9dfe
Chrimle <28791817+Chrimle@users.noreply.github.com>
2025-09-02 19:30:03 +0200

    Set All Google Analytics Consent to 'Denied' by Default

--- scripts/google-analytics.js
+++ scripts/google-analytics.js
@@ -10,6 +10,15 @@ export function loadGoogleAnalytics() {
         window.dataLayer = window.dataLayer || [];
         function gtag() { dataLayer.push(arguments); }
         gtag('js', new Date());
+        gtag('consent', 'default', {
+            'ad_storage': 'denied',
+            'ad_user_data': 'denied',
+            'ad_personalization': 'denied',
+            'analytics_storage': 'denied',
+            'functionality_storage': 'denied',
+            'personalization_storage': 'denied',
+            'security_storage': 'denied'
+        });
         gtag('consent', 'update', {
             'analytics_storage': GoogleAnalytics.isAnalyticsConsented() ? 'granted' : 'denied'
         });
